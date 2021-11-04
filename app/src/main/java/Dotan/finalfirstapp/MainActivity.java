533bfd649381f73e4892d92ad5c36f142198685f
Beckdotan <dotan.beck@gmail.com>
2021-11-04 21:23:31 +0200

    app working, with 2 buttons and 2 screens with lists without any text

---
+++ app/src/main/java/Dotan/finalfirstapp/MainActivity.java
@@ -0,0 +1,46 @@
+package Dotan.finalfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 =(Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+    }
+
+    public void openActivity1(){
+        Intent intent = new Intent(this,Activity1.class);
+        startActivity(intent);
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+    }
+
+}
