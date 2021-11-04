fe34a4f14d458526b5dc2c684e75b8348438652c
Beckdotan <dotan.beck@gmail.com>
2021-11-04 22:29:09 +0200

    few unseccesful trise are her as well in page 1 to use the adapter

--- app/src/main/java/Dotan/finalfirstapp/Activity1.java
+++ app/src/main/java/Dotan/finalfirstapp/Activity1.java
@@ -12,9 +12,31 @@ public class Activity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_1);
-        String[] lst =  {"a","b","c","d"};
-        RecyclerView lst1 = (RecyclerView) findViewById(R.id.RecyclerView);
-        ListAdapter list = new ListAdapter(lst);
+        String[] lst =  {"a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",
+                "a","b","c","d",};
+
+
+        //Somthing in those things make the app collaps when i press button 1
+
+       // RecyclerView mlist = (RecyclerView) findViewById(R.id.RecyclerView);
+      //  mlist.setAdapter(new ListAdapter(lst));
+
+       // RecyclerView lst1 = (RecyclerView) findViewById(R.id.RecyclerView);
+       // ListAdapter list = new ListAdapter(lst);
+      //  RecyclerView view = (RecyclerView) findViewById(R.id.RecyclerView);
+      //  view.setAdapter(list);
+
     }
 }
